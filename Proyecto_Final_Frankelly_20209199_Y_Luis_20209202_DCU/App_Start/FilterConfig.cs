e69410994286309b740e6cb7fc9e9435e577740c
Luis Guzman <luisguzman271001@gmail.com>
2021-08-26 09:30:55 -0400

    Add project files.

---
+++ Proyecto_Final_Frankelly_20209199_Y_Luis_20209202_DCU/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Final_Frankelly_20209199_Y_Luis_20209202_DCU
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Proyecto_Final_Frankelly_20209199_Y_Luis_20209202_DCU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Final_Frankelly_20209199_Y_Luis_20209202_DCU.MvcApplication" Language="C#" %>
